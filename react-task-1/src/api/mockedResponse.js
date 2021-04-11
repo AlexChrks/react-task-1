9b13a40787effd47794c35a34821fe84f9c76671
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2021-04-11 16:47:51 +0300

    feat: implement second react task. Realize card component and cardsContainer component

---
+++ react-task-1/src/api/mockedResponse.js
@@ -0,0 +1,50 @@
+export const mockedResponse = [
+  {
+    id: 1,
+    price: 15,
+    title: 'Logitech K120',
+    type: 'Keyboard',
+    imageUrl: 'https://www.b2b-center.ru/public/images/tuvers/products/big/ff/3a/37/251941887.jpeg',
+    country: 'China'
+  },
+  {
+    id: 2,
+    price: 20,
+    title: 'Asus 15',
+    type: 'Laptop',
+    imageUrl: 'https://avatars.mds.yandex.net/get-mpic/1901070/img_id3896220268836876630.jpeg/orig',
+    country: 'Russia'
+  },
+  {
+    id: 3,
+    price: 200,
+    title: 'LG 27MK600m',
+    type: 'Monitor',
+    imageUrl: 'https://www.lg.com/us/images/monitors/md05998958/gallery/MK600M-D1.jpg',
+    country: 'Korea'
+  },
+  {
+    id: 4,
+    price: 800,
+    title: 'Dell Inspiron 5501',
+    type: 'Laptop',
+    imageUrl: 'https://img.5element.by/import/images/ut/goods/good_7477e928-06ea-11eb-80c1-005056844aec/5501-213309-inspiron-noutbuk-dell-3_600.jpg',
+    country: 'China'
+  },
+  {
+    id: 5,
+    price: 1800,
+    title: 'Macbook Pro 13',
+    type: 'Laptop',
+    imageUrl: 'https://app-minsk.by/files/items/2922/icon_480x390.jpg',
+    country: 'USA'
+  },
+  {
+    id: 6,
+    price: 1300,
+    title: 'Macbook Air 13',
+    type: 'Laptop',
+    imageUrl: 'https://www.notebookcheck-ru.com/fileadmin/Notebooks/Apple/MacBook_Air_13_Mid_2014/air13teaser.jpg',
+    country: 'USA'
+  },
+];
